915fe33a28dcaca56b1fa424d0b9c16e51d31aff
minhphan46 <21522345@gm.uit.edu.vn>
2023-01-12 22:55:16 +0700

    add email

--- lib/app/data/models/student.dart
+++ lib/app/data/models/student.dart
@@ -1,18 +1,23 @@
 class Student {
+  // them thong tin email phu huynh
   String? id;
   String name;
   int? numOfCorrect;
   int? numOfWrong;
   String? imageAvartaUrl;
+  String? parentEmail;
 
-  Student(
-      {this.id,
-      required this.name,
-      this.numOfCorrect,
-      this.numOfWrong,
-      this.imageAvartaUrl}) {
+  Student({
+    this.id,
+    required this.name,
+    this.numOfCorrect,
+    this.numOfWrong,
+    this.imageAvartaUrl,
+    this.parentEmail,
+  }) {
     id ??= DateTime.now.toString();
     numOfCorrect ??= 0;
     numOfWrong ??= 0;
+    parentEmail ??= "phanvanminh1234567890@gmail.com";
   }
 }
