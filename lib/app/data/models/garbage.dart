ed112cad2bdfb60816ec30cca3a9cc59ffe599b3
minhphan46 <21522345@gm.uit.edu.vn>
2023-02-18 17:30:13 +0700

    garbage model

---
+++ lib/app/data/models/garbage.dart
@@ -0,0 +1,26 @@
+class Garbage {
+  String? id; // id of trash
+  String idStudent; // id of student threw trash
+  DateTime? dateCreate; // date create of comment
+  bool isRight; // true: right, false: Wrong
+
+  Garbage({
+    this.id,
+    required this.idStudent,
+    this.dateCreate,
+    required this.isRight,
+  }) {
+    id ??= DateTime.now().toString();
+    dateCreate ??= DateTime.now();
+  }
+
+  //fetch garbage from json
+  factory Garbage.fromJson(Map<String, dynamic> json) {
+    return Garbage(
+      id: json['id'],
+      idStudent: json['idStudent'],
+      isRight: json['isRight'],
+      dateCreate: json['dateCreate'],
+    );
+  }
+}
